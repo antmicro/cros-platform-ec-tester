f2babd5787789fd7aa91327c09c97a2bdc276bac
Jan Malek <jmalek@antmicro.com>
2024-10-23 08:29:02 +0200

    ci: Allow building Zephyr to fail

--- .github/scripts/copy_helpers.sh
+++ .github/scripts/copy_helpers.sh
@@ -7,5 +7,6 @@ copy_artifacts() {
 copy_artifacts_zephyr() {
     # $1 - board name
     mkdir -p "artifacts/$1"
-    find "artifacts-raw/build/zephyr/$1/output" \( -iname '*.bin' -o -iname '*.ro.elf' -o -iname '*.rw.elf' \) -exec cp '{}' "artifacts/$1" \;
+    # Fetching build dependencies for Zephyr based targets is flaky on CI. Don't fail the entire pipeline if the binaries couldn't be built
+    find "artifacts-raw/build/zephyr/$1/output" \( -iname '*.bin' -o -iname '*.ro.elf' -o -iname '*.rw.elf' \) -exec cp '{}' "artifacts/$1" \; || true
 }
